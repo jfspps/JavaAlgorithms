4c58e20039835431582619d92ac8e94690d43e50
jfspps <jfsapps@gmail.com>
2020-11-11 17:44:02 +0000

    updated READMEs and added Stack and Queue classes

---
+++ src/StacksAndQueues/algorithms/Stack.java
@@ -0,0 +1,53 @@
+package StacksAndQueues.algorithms;
+
+import linkedList.algorithms.Palindrome;
+
+import java.util.EmptyStackException;
+
+public class Stack<T> {
+
+    // when instantiating a Stack, the respective nodes are private and accessible/viewed via pop(), push(), peek() and isEmpty()
+    // note that the Stack and Queue are implemented here as linked lists to allow for dynamic allocation (arrays may be used)
+    private static class StackNode<T> {
+        private T data;
+        private StackNode<T> next;
+
+        public StackNode(T data){
+            this.data = data;
+        }
+    }
+
+    // references the top of the stack (last node in, first node out)
+    private StackNode<T> top;
+
+    // removes a new Node and updates top
+    public T pop(){
+        if (top == null){
+            throw new EmptyStackException();
+        }
+
+        T item = top.data;
+        top = top.next;
+        return item;
+    }
+
+    // adds an item as the top node
+    public void push(T item){
+        StackNode<T> newItem = new StackNode<>(item);
+        newItem.next = top;
+        top = newItem;
+    }
+
+    // returns the current top item
+    public T peek(){
+        if (top == null){
+            throw new EmptyStackException();
+        }
+        return top.data;
+    }
+
+    // checks if a stack is empty or not
+    public boolean isEmpty(){
+        return top == null;
+    }
+}
